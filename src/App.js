4ce8281f55bfe2db8925c646da6c92019c7e4014
Masa Utkovic <masa.utkovic@gmail.com>
2021-01-08 10:47:01 +0100

    add display component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import "./App.css";
 import Buttons from "./Buttons";
+import Display from "./Display";
 
 class App extends React.Component {
 
   render() {
     return (
       <div>
+        <h1>JS Calculator</h1>
+        <Display/>
         <Buttons/>
       </div>
     )
---
+++ src/Display.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Display extends React.Component {
+ 
+    render() {
+        return(
+            <div id="display"><h2>This will be the output area</h2></div>
+        )
+    }
+}
+
+export default Display;
